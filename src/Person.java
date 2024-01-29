61f6930102676c91b3e0063e3ac6901e7627a912
OPJKP <74258094+OPJKP@users.noreply.github.com>
2024-01-29 14:55:00 +0100

    zz

--- src/Person.java
+++ src/Person.java
@@ -1,72 +1,8 @@
 import java.util.List;
 
+
 public class Person {
     String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isSafe() {
-        return isSafe;
-    }
-
-    public void setSafe(boolean safe) {
-        isSafe = safe;
-    }
-
-    public boolean isPeutInfecter() {
-        return peutInfecter;
-    }
-
-    public void setPeutInfecter(boolean peutInfecter) {
-        this.peutInfecter = peutInfecter;
-    }
-
-    public boolean isDead() {
-        return isDead;
-    }
-
-    public void setDead(boolean dead) {
-        isDead = dead;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public List<String> getVariants() {
-        return variants;
-    }
-
-    public void setVariants(List<String> variants) {
-        this.variants = variants;
-    }
-
-    public List<Person> getRelation() {
-        return relation;
-    }
-
-    public void setRelation(List<Person> relation) {
-        this.relation = relation;
-    }
-
-    public Person getAmiConnexe() {
-        return amiConnexe;
-    }
-
-    public void setAmiConnexe(Person amiConnexe) {
-        this.amiConnexe = amiConnexe;
-    }
-
     boolean isSafe;
     boolean peutInfecter;
     boolean isDead;
